b21790951488eb43b1ae955e9298c629629c5aee
Daniely Vitoria Dallagrana <103067233+yyy1212@users.noreply.github.com>
2023-11-24 08:53:45 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,8 +2,19 @@ function tocaSomPom () {
    document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelectorAll ('.tecla')
 
-listaDeTeclas
+ const listaDeTeclas = document.querySelectorAll ('.tecla');
 
+listaDeTeclas[0].onclick = tocaSomPom;
 
+let contador = 0;
+
+//enquanto
+while (contador = 9) {
+
+   listaDeTeclas[contador].onclick = tocaSomPom; 
+
+   contador = contador + 1;
+
+   console.log(contador);
+}
